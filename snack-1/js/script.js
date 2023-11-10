ba2252885aa2c191e1914a454f1856e675f9db4b
RicTieri <tieri.riccardo@gmail.com>
2023-11-10 11:57:02 +0100

    try

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,6 +1,8 @@
 const ageUserOne = parseInt(prompt('inserisci la tuà età'))
 const ageUserTwo = parseInt(prompt('inserisci la tuà età'))
 
-if (ageUserOne>ageUserTwo){
-  console.log(utente più anziano ha: 'ageUserOne' )
+if (ageUserOne > ageUserTwo){
+  console.log(ageUserOne)
+} else if (ageUserTwo > ageUserTwo){
+  console.log(ageUserTwo)
 }
