38cec2dca34767de59e2cc2aa840a1b8f4a73c95
RicTieri <tieri.riccardo@gmail.com>
2023-11-10 11:52:47 +0100

    first commit

---
+++ snack-1/js/script.js
@@ -0,0 +1,6 @@
+const ageUserOne = parseInt(prompt('inserisci la tuà età'))
+const ageUserTwo = parseInt(prompt('inserisci la tuà età'))
+
+if (ageUserOne>ageUserTwo){
+  console.log(utente più anziano ha: 'ageUserOne' )
+}
