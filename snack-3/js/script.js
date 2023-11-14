836dcb7ca355a8835192f2eb317af023bafce9f6
RicTieri <tieri.riccardo@gmail.com>
2023-11-14 12:39:41 +0100

    snack-3

---
+++ snack-3/js/script.js
@@ -0,0 +1,16 @@
+const wrapperElement = document.querySelector('div.wrapper');
+
+for ( let i = 0; i <=20 ; i = i + 2){
+  const cardElement = document.createElement('div');
+  cardElement.className = 'card';
+
+  const headingCardElement = document.createElement('h2');
+  headingCardElement.innerHTML = i;
+
+  cardElement.appendChild(headingCardElement);
+  wrapperElement.appendChild(cardElement);
+
+  console.log(i)
+
+}
+
