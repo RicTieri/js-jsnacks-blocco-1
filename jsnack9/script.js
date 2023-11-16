61992d6f469baa954b9659cc4ae3f9aad8e1858b
RicTieri <tieri.riccardo@gmail.com>
2023-11-16 16:03:43 +0100

    added variable

--- jsnack9/script.js
+++ jsnack9/script.js
@@ -1,2 +1,11 @@
 // Chiedi all'utente due numeri, uno piu' piccolo (due cifre) e uno piu' grande (almeno quattro cifre):
+const twoDigits = parseInt(prompt('inserisci un numero a due cifre'));
+const fourDigits = parseInt(prompt('inserisci un numero a quattro cifre'));
+let result;
 // moltiplica il primo numero per 2 finche' non arriva ad essere maggiore del secondo numero.
+
+if (fourDigits > twoDigits){
+  result = twoDigits * 2
+}
+
+console.log(result)
