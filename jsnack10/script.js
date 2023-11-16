a0e7eb3f0f61071cbe43ea1b7967bc5772f22c09
RicTieri <tieri.riccardo@gmail.com>
2023-11-16 16:18:32 +0100

    set file snack10

---
+++ jsnack10/script.js
@@ -0,0 +1,3 @@
+// Inserisci un <h1> al centro della pagina con un numero randomico all'interno e un bottone sotto.
+
+// Ad ogni click del bottone aumento il numero nell'h1 di 3 e lo aggiorno in pagina. 
