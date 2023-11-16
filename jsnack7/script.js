689076225400a351f615326781f26399170d1559
RicTieri <tieri.riccardo@gmail.com>
2023-11-16 15:28:07 +0100

    initialized folder

---
+++ jsnack7/script.js
@@ -0,0 +1,3 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all'utente di inserire un numero
+// controllare se è dispari se si inseriscilo nell'array.
---
+++ jsnack8/script.js
@@ -0,0 +1,2 @@
+// Chiedi un numero di 7 cifre all'utente 
+// calcola la somma di tutte le cifre che compongono il numero.
