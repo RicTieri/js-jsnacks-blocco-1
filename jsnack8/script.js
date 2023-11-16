5e5283194f2f071967b785095e8f957c8dcee90c
RicTieri <tieri.riccardo@gmail.com>
2023-11-16 15:56:08 +0100

    added array item sum

--- jsnack8/script.js
+++ jsnack8/script.js
@@ -1,5 +1,12 @@
 // Chiedi un numero di 7 cifre all'utente 
-const newNumber = parseInt(prompt('Inserire un numero a 7 cifre'));
+const newNumber = prompt('Inserire un numero a 7 cifre');
 // calcola la somma di tutte le cifre che compongono il numero.
-const array = Array.from(newNumber.toString()).map(Number);
+const array = Array.from(newNumber).map(Number);
+let sum = 0;
 console.log(array)
+
+for (let i=0; i<array.length; i++){
+  sum += array[i]
+}
+
+console.log(sum)
