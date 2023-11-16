ad45d44a3b1ad418bf1ac0c89a4f1f376fd6a621
RicTieri <tieri.riccardo@gmail.com>
2023-11-16 15:48:56 +0100

    added separetion in digits

--- jsnack8/script.js
+++ jsnack8/script.js
@@ -1,2 +1,5 @@
 // Chiedi un numero di 7 cifre all'utente 
+const newNumber = parseInt(prompt('Inserire un numero a 7 cifre'));
 // calcola la somma di tutte le cifre che compongono il numero.
+const array = Array.from(newNumber.toString()).map(Number);
+console.log(array)
